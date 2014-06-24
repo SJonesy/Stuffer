d71d9002ff9a8894f46ea4be5872355ca9054396
Stephen Jones <stephen.jones@whiteboard-it.com>
2014-06-23 19:47:57 -0500

    get tables and columns

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
-var mysql      = require ('mysql')
-  , settings   = require ('./settings');
+var mysql    = require ('mysql')
+  , settings = require ('./settings')
+  , q        = require ('q');
 
 var connection = mysql.createConnection(settings.db);
 
 connection.connect();
 
-var query = function (sql, parameters, connection) {
+
+return query("SHOW TABLES", [], connection)
+.then(function (tables) {
+
+  // put table names in array
+  tables = tables.map(function (table) {
+    return table['Tables_in_' + settings.db.database];
+  });
+
+  tables.forEach(function (table) {
+    fillTable(table);
+  });
+
+});
+
+
+// promisfied mysql.query function
+function query (sql, parameters, connection) {
   var defer = q.defer();
 
   connection.query(sql, parameters, function (err, result) {
@@ -19,3 +37,22 @@ var query = function (sql, parameters, connection) {
   return defer.promise;
 }
 
+function fillTable (table) {
+  return getColumns(table)
+  .then(function (columns) {
+
+    console.log(columns);
+
+  });
+}
+
+function getColumns (table) {
+  var sql = "SHOW COLUMNS FROM " + table;
+
+  return query(sql, [], connection)
+  .then(function (columns) {
+
+    return q(columns);
+
+  });
+}
