b810e6ae6549e34e3d7e8738dcba663f9f8733c7
Stephen Jones <stephen.jones@whiteboard-it.com>
2014-06-23 19:10:57 -0500

    add query function

--- app.js
+++ app.js
@@ -5,5 +5,17 @@ var connection = mysql.createConnection(settings.db);
 
 connection.connect();
 
+var query = function (sql, parameters, connection) {
+  var defer = q.defer();
 
+  connection.query(sql, parameters, function (err, result) {
+    if (err) {
+      console.log('mysql query error: ', err);
+      return defer.reject(err)
+    }
+    defer.resolve(result);
+  });
+
+  return defer.promise;
+}
 
